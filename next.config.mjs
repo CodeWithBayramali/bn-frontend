9051190b317f7f55b76f02065590cb53eb7478da
CodeWithBayramali <bdurak3@gmail.com>
2024-09-27 15:38:19 +0300

    proxy-completed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+          {
+            source: '/api/:path*', // API istekleri için proxy tanımı
+            destination: 'http://185.9.37.6:8080/api/:path*', // Backend'in HTTP adresi
+          },
+        ];
+      },
+};
 
 export default nextConfig;
