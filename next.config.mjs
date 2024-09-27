018deaea6e9e93177efa77fcacb472d9593eae27
CodeWithBayramali <bdurak3@gmail.com>
2024-09-27 17:34:44 +0300

    hdartk

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async rewrites() {
-        return [
-          {
-            source: '/v1/api:path*', // API istekleri için proxy tanımı
-            destination: 'http://185.9.37.6:8080/v1/api:path*', // Backend'in HTTP adresi
-          },
-        ];
-      },
-};
+const nextConfig = {};
 
 export default nextConfig;
